8d5875c7e15deb8f02186c47c9490e523b6320db
Candy Dong <qingyid@andrew.cmu.edu>
2018-03-19 13:58:49 -0400

    narration txt file generated

--- missing_letters/miss_letter.py
+++ missing_letters/miss_letter.py
@@ -34,6 +34,19 @@ def read_spreadsheet(excel_path):
 
 
 
+#read file names in narration path and put them into a txt file 
+#which is under the same direction as this python script
+def read_narration(narration_folder_path, narration_txt_path):
+	with open(narration_txt_path, 'w') as narration_txt:
+		result_str = ""
+		for filename in os.listdir(narration_folder_path):
+			result_str += filename
+			result_str += "\n"
+		narration_txt.write(result_str)
+	
+
+
+		
 #sys.argv[1] = path of txt file which contains all data source names
 def main():
     #there should be only one command line argument 
@@ -43,6 +56,14 @@ def main():
 
     excel_path = sys.argv[1]
     content_list = read_spreadsheet(excel_path)
+
+    narration_folder_path = "./Initial_syllable_narration"
+    narration_txt_path = "./Initial_syllable_narration.txt"
+    read_narration(narration_folder_path, narration_txt_path)
+
+    return
+
+
 
     for content_dic in content_list:
         filename = content_dic['Name']
