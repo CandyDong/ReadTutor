a7c30417bf73793134066328fa99c0ac2ddf0c87
Candy Dong <qingyid@andrew.cmu.edu>
2018-03-19 17:39:02 -0400

    miss_letter update
    read_narration finished

--- missing_letters/miss_letter.py
+++ missing_letters/miss_letter.py
@@ -36,13 +36,60 @@ def read_spreadsheet(excel_path):
 
 #read file names in narration path and put them into a txt file 
 #which is under the same direction as this python script
-def read_narration(narration_folder_path, narration_txt_path):
-	with open(narration_txt_path, 'w') as narration_txt:
+def read_narration(audio_path, txt_path):
+	#store all the names of narration folders
+	narration_folder_names = []
+	for narration_folder in os.listdir(audio_path):
+		if narration_folder == '.DS_Store':
+			continue
+		narration_folder_names.append(narration_folder)
+	narration_folder_names.sort() #sorted increasingly (starts from prior_1)
+
+	#loop through all the narration files and 
+	#put names of the files into a single txt file
+	#specified by txt_path
+	with open(txt_path, 'w') as txt_file:
 		result_str = ""
-		for filename in os.listdir(narration_folder_path):
-			result_str += filename
-			result_str += "\n"
-		narration_txt.write(result_str)
+		for folder in narration_folder_names:
+			folder_path = audio_path + "/" + folder
+			print(folder_path)
+			for filename in os.listdir(folder_path):
+
+				#eliminate files that are not mp3 or wav
+				if ((not filename.endswith(".mp3")) and
+				   (not filename.endswith(".wav"))):
+					continue
+
+				#delete suffix like ".mp3" and ".wav"
+				suffix_index = filename.find(".")
+				filename = filename[:suffix_index]
+
+				#delete "copy of" if it's in the filename
+				if filename.startswith("Copy of"):
+					filename_list = filename.split(" ")
+					filename = filename_list[2]
+
+				#delete (1) if it's in the filename
+				if filename.endswith("(1)"):
+					num_index = filename.find("(1)")
+					filename = filename[:num_index]
+
+				#eliminate phrase
+				if len(filename.split(" ")) > 1:
+					continue
+
+				#convert all the letters to lowercase
+				filename = filename.lower()
+
+				#eliminate words that have already been included
+				
+				if filename[:suffix_index] in result_str:
+					continue
+				result_str += filename
+				result_str += "\n"
+			
+		txt_file.write(result_str)
+
 
 
 		
@@ -56,17 +103,15 @@ def main():
     excel_path = sys.argv[1]
     content_list = read_spreadsheet(excel_path)
 
-    narration_folder_path = "./Initial_syllable_narration"
-    narration_txt_path = "./Initial_syllable_narration.txt"
-    read_narration(narration_folder_path, narration_txt_path)
+    audio_path = "./narration"
+    txt_path = "./word_data.txt"
+    read_narration(audio_path, txt_path)
 
     return
 
-
-
     for content_dic in content_list:
         filename = content_dic['Name']
-        write_file(filename, content_dic)
+        write_file(filename, content_dic, narration_txt_path)
 
 
 
